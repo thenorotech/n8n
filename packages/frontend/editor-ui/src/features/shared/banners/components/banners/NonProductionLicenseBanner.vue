159e91c43a4d1897b47c9680a9b5e95e4e038812
David Sanchez <davidsnjaz@gmail.com>
2025-04-22 16:20:29 -0600

    Change Workflow

--- packages/frontend/editor-ui/src/features/shared/banners/components/banners/NonProductionLicenseBanner.vue
+++ packages/frontend/editor-ui/src/features/shared/banners/components/banners/NonProductionLicenseBanner.vue
@@ -4,9 +4,10 @@ import { i18n as locale } from '@n8n/i18n';
 </script>
 
 <template>
-	<BaseBanner name="NON_PRODUCTION_LICENSE" :dismissible="false">
+	<!-- BaseBanner name="NON_PRODUCTION_LICENSE" :dismissible="false">
 		<template #mainContent>
 			<span>{{ locale.baseText('banners.nonProductionLicense.message') }}</span>
 		</template>
-	</BaseBanner>
+	</BaseBanner -->
+	<div></div>
 </template>
