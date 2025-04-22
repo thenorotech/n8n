58accf04c8f1ba40a6f3f5a41299a4a52cdb9349
David Sanchez <davidsnjaz@gmail.com>
2025-04-22 14:45:45 -0600

    Change license

--- packages/cli/src/license.ts
+++ packages/cli/src/license.ts
@@ -413,7 +413,7 @@ export class License implements LicenseProvider {
 	}
 
 	getPlanName(): string {
-		return this.getValue('planName') ?? 'Community';
+		return 'Enterprise';
 	}
 
 	getInfo(): string {
